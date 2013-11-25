979d04fbd78df4a13f1df0eee66e304b3d743edf
Josh Barr <josh@springload.co.nz>
2013-11-25 18:16:35 +1300

    added conditionals

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ Pretty select boxes for work and pleasure.
 ### Select
 ```html
  <div class='field_select' data-select>
-    <select class="field_select__input" id="mySelect" name="mySelect" tabindex="">
-        <option value="">Option 1</option>
-        <option value="" selected>Option 2</option>
-        <option value="" selected>Option 3</option>
-        <option value="" selected>Option 4</option>
+    <select class="field_select__input" id="mySelect" name='mySelect' tabindex=''>
+        <option value=''>Option 1</option>
+        <option value='' selected>Option 2</option>
+        <option value='' selected>Option 3</option>
+        <option value='' selected>Option 4</option>
     </select>
     <div data-select-label='mySelect' class='field_select__value field field_text'></div>
     <div class='field_select__affordance'>
@@ -24,8 +24,19 @@ Pretty select boxes for work and pleasure.
 <div class='icon_text'>
     <div class='i field_checkbox' data-checkbox data-id='myCheckbox'>
         <i class='field_checkbox__fancy'></i>
-        <input type='checkbox' id='myCheckbox' name='myCheckbox' value='1' tabindex="">
+        <input type='checkbox' id='myCheckbox' name='myCheckbox' value='1' tabindex=''>
         <label class='icon_text__label field_checkbox__label' for='myCheckbox'>My awesome checkbox</label>
     </div>
 </div>
+```
+
+### Conditionals
+
+```html
+<input type='text' placeholder='Your weight in kilogrammes' data-additional-trigger='motivation' data-additional-value='>100'>
+
+<div data-additional='motivation'>
+    Maybe time to lay off the pies, buddy!
+</div>
+
 ```
--- fancy-inputs.js
+++ fancy-inputs.js
@@ -1,135 +1,249 @@
-var FancyInput = (function(){
+var Fancy = (function(document,window){
 
-    var hasJS = function() {
-        var root = document.documentElement;
-        root.className = root.className.replace(/\bno-js\b/,'') + ' js';
-    };
+    var emptyFormElem = function($elem) {
 
-    var Fancy = function(options){
-        var self = this;
-        this.element = "";
-        this.input = "";
-        this.triggerElement = "";
-        if (arguments.length) {
-            $.extend(self, options);
+        // Selects (set to top item)
+        if ($elem.prop("tagName") === "SELECT") {
+            $elem.children("option").attr("selected", false);
+            $elem.children("option:first-child").attr("selected", true);
+            $elem.trigger("change");
         }
-        return self;
+
+        // Textareas
+        else if ($elem.prop("tagName") === "TEXTAREA") {
+            $elem.html("");
+            $elem.trigger("change");
+        }
+
+        // Everything else
+        else if ($elem.prop("tagName") === "INPUT") {
+            // Everything else
+            if ($elem.attr("type") !== "checkbox" && $elem.attr("type") !== "radio") {
+                $elem.val("");
+            } else {
+                $elem.attr("checked", false);
+            }
+            $elem.trigger("change");
+        }
+
+        // Make it recursive
+        var $children = $elem.children();
+        if ($children.size() > 0) {
+            $children.each(function(index, elem){
+                emptyFormElem($(elem));
+            });
+        }
+
+        return;
     };
 
-    Fancy.prototype = {
-        init: function(){
-            var self = this,
-                $inputs = $(self.element);
 
-            $inputs.each(function(i) {
-                var $domEl = $(this);
-                var $inputEl = $domEl;
-                if (self.input) {
-                    $inputEl = $(this).find(self.input);
-                }
-                $inputEl.on("change keyup", function(e){
+    var FancyInput = (function(){
 
-                    var target = e.target,
-                        $target = $(target);
-                    self.updateState($target, $domEl);
+        var hasJS = function() {
+            var root = document.documentElement;
+            root.className = root.className.replace(/\bno-js\b/,'') + ' js';
+        };
+
+        var Fancy = function(options){
+            var self = this;
+            this.element = "";
+            this.input = "";
+            this.triggerElement = "";
+            if (arguments.length) {
+                $.extend(self, options);
+            }
+            return self;
+        };
+
+        Fancy.prototype = {
+            init: function(){
+                var self = this,
+                    $inputs = $(self.element);
+
+                $inputs.each(function(i) {
+                    var $domEl = $(this);
+                    var $inputEl = $domEl;
+                    if (self.input) {
+                        $inputEl = $(this).find(self.input);
+                    }
+                    $inputEl.on("change keyup", function(e){
+
+                        var target = e.target,
+                            $target = $(target);
+                        self.updateState($target, $domEl);
+                    });
+
+                    if (self.triggerElement) {
+                        $(self.triggerElement, $domEl).on("click", function(e) {
+                            $inputEl.trigger("click");
+                        });
+                    }
+
+                    if (self.onInputInitialize) {
+                        self.onInputInitialize.call(self, $domEl, $inputEl);
+                    }
+
+                    if (!$domEl.data("ignore-onload")) {
+                        self.updateState($inputEl, $domEl);
+                    }
+
                 });
 
-                if (self.triggerElement) {
-                    $(self.triggerElement, $domEl).on("click", function(e) {
-                        $inputEl.trigger("click");
-                    });
+                if (self.onInitialize) {
+                    self.onInitialize.call(self);
                 }
-
-                if (self.onInputInitialize) {
-                    self.onInputInitialize.call(self, $domEl, $inputEl);
-                }
-
-                if (!$domEl.data("ignore-onload")) {
-                    self.updateState($inputEl, $domEl);
-                }
-
-            });
-
-            if (self.onInitialize) {
-                self.onInitialize.call(self);
-            }
-        },
-        updateState: function($el, $domEl) {
-            var self = this;
-            if(self.onInputChange) {
-                self.onInputChange.call(self, $el, $domEl);
-            }
-        }
-    };
-    hasJS();
-    return Fancy;
-})();
-
-/**
- * SelectBoxes
- * Update an arbitrary label element based on the selectedIndex.
- */
-var FancySelects = (function(FancyInput) {
-    var Selects = new FancyInput({
-        element: "[data-select]",
-        input: "select",
-        activeClass: "select--loaded",
-        debug: "foo",
-        onInputInitialize: function($domEl, $inputEl) {
-            $domEl.parent().addClass(self.activeClass);
-            $domEl.find("[data-select-label]").on("click", function(){
-                $inputEl.focus();
-            });
-        },
-        onInputChange: function($inputEl, $domEl) {
-            var self = this,
-                $self = $inputEl,
-                domEl = $self.get(0),
-                index = $self.prop("selectedIndex"),
-                labelText = "",
-                newLabelText = "&nbsp;";
-
-            var labelEl = $inputEl.siblings("[data-select-label]").get(0);
-
-            if(domEl.options.length) {
-                var optionText = domEl.options[index].text;
-                if (optionText !== "") {
-                    newLabelText = optionText;
+            },
+            updateState: function($el, $domEl) {
+                var self = this;
+                if(self.onInputChange) {
+                    self.onInputChange.call(self, $el, $domEl);
                 }
             }
+        };
+        hasJS();
+        return Fancy;
+    })();
 
-            labelText = newLabelText;
-            labelEl.innerHTML = labelText;
-        }
+    /**
+     * SelectBoxes
+     * Update an arbitrary label element based on the selectedIndex.
+     */
+    var FancySelects = (function(FancyInput) {
+        var Selects = new FancyInput({
+            element: "[data-select]",
+            input: "select",
+            activeClass: "select--loaded",
+            debug: "foo",
+            onInputInitialize: function($domEl, $inputEl) {
+                $domEl.parent().addClass(self.activeClass);
+                $domEl.find("[data-select-label]").on("click", function(){
+                    $inputEl.focus();
+                });
+            },
+            onInputChange: function($inputEl, $domEl) {
+                var self = this,
+                    $self = $inputEl,
+                    domEl = $self.get(0),
+                    index = $self.prop("selectedIndex"),
+                    labelText = "",
+                    newLabelText = "&nbsp;";
+
+                var labelEl = $inputEl.siblings("[data-select-label]").get(0);
+
+                if(domEl.options.length) {
+                    var optionText = domEl.options[index].text;
+                    if (optionText !== "") {
+                        newLabelText = optionText;
+                    }
+                }
+
+                labelText = newLabelText;
+                labelEl.innerHTML = labelText;
+            }
+        });
+        return Selects;
+    })(FancyInput);
+
+    /**
+     * Pretty checkboxes
+     */
+    var FancyCheckboxes = (function(FancyInput){
+        var Checkboxes = new FancyInput({
+            element: "[data-checkbox]",
+            input: "input",
+            triggerElement: ".field_checkbox__fancy",
+            activeClass: "field_checkbox--checked",
+            onInputChange: function($el) {
+                var self = this,
+                    el = $el.get(0),
+                    $parent = $el.parent();
+                if (el.checked) {
+                    $parent.addClass(self.activeClass);
+                } else {
+                    $parent.removeClass(self.activeClass);
+                }
+            }
+        });
+        return Checkboxes;
+    })(FancyInput);
+
+
+    var FancyConditionals = (function(FancyInput){
+        var showHide = new FancyInput({
+            element: "[data-additional-trigger]",
+            input: "select, input",
+            debug: "bar",
+            onInitialize: function() {
+                var self = this;
+                var $links = $(self.element).not("[data-additional-value]");
+
+                $links.on("click change", function(e) {
+
+                    var target = e.currentTarget;
+                    var $target = $(target);
+                    if ($target.data("hide-self")) {
+                        $target.hide();
+                    }
+
+                    self.onInputChange.call(self, null, $target);
+                });
+            },
+            onInputChange: function($inputEl, $domEl){
+                var self = this;
+                var validationCondition = $domEl.data("additional-value");
+                var additionalTrigger = $domEl.data("additional-trigger");
+                var hasInput = $inputEl ? true : false;
+                if (hasInput) {
+
+                    var show = false;
+                    // different operators
+
+                    // less than
+                    if (validationCondition.toString().substring(0,1) == "<") {
+                        if ($.isNumeric($inputEl.val()) && parseInt($inputEl.val()) < parseInt(validationCondition.substring(1))) {
+                            show = true;
+                        }
+                        // greater than
+                    } else if ($.isNumeric($inputEl.val()) && validationCondition.toString().substring(0,1) == ">") {
+                        if ($inputEl.val() > validationCondition.substring(1)) {
+                            show = true;
+                        }
+                        // equal to
+                    } else if ($inputEl.val() == validationCondition) {
+                        show = true;
+                    }
+                } else {
+                    show = true;
+                }
+
+                if (show) {
+                    self.showAdditional(additionalTrigger);
+                } else {
+                    self.hideAdditional(additionalTrigger);
+                }
+                // Fire an event in case we need to do other stuff
+                $(document).trigger("inputChange" + additionalTrigger);
+            },
+            showAdditional: function(formElementName) {
+                $("[data-additional='" + formElementName + "']").removeAttr("data-additional-hide");
+            },
+            hideAdditional: function(formElementName) {
+                var $elem = $("[data-additional='" + formElementName + "']");
+                $elem.attr("data-additional-hide", "true");
+                this.emptyValues($elem);
+            },
+            emptyValues: function($elem) {
+                emptyFormElem($elem);
+            }
+        });
+        return showHide;
+    })(FancyInput);
+
+    $(document).ready(function() {
+        FancySelects.init();
+        FancyCheckboxes.init();
+        FancyConditionals.init();
     });
-    return Selects;
-})(FancyInput);
 
-/**
- * Pretty checkboxes
- */
-var FancyCheckboxes = (function(FancyInput){
-    var Checkboxes = new FancyInput({
-        element: "[data-checkbox]",
-        input: "input",
-        triggerElement: ".field_checkbox__fancy",
-        activeClass: "field_checkbox--checked",
-        onInputChange: function($el) {
-            var self = this,
-                el = $el.get(0),
-                $parent = $el.parent();
-            if (el.checked) {
-                $parent.addClass(self.activeClass);
-            } else {
-                $parent.removeClass(self.activeClass);
-            }
-        }
-    });
-    return Checkboxes;
-})(FancyInput);
-
-
-$(document).ready(function() {
-    FancySelects.init();
-    FancyCheckboxes.init();
-});
+})(document,window);
